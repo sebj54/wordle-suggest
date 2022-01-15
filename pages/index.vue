92df0c17424edfcb56f76be4300fa7716a540ea4
Sébastien JEAN <contact@sebjean.com>
2022-01-15 01:10:55 +0100

    ✨ Add english dictionary

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,20 @@
 </template>
 
 <script>
-import dictionary from '@/dictionaries/fr.json'
+import dictionaryEn from '@/dictionaries/en.json'
+import dictionaryFr from '@/dictionaries/fr.json'
 
 export default {
     data() {
         return {
-            dictionary,
+            dictionary: [],
+        }
+    },
+    fetch() {
+        if (this.$i18n.locale === 'fr') {
+            this.dictionary = dictionaryFr
+        } else {
+            this.dictionary = dictionaryEn
         }
     },
 }
