1ef651e4ac1fa9a6db7f1be540f2b9ce9d4501b5
Sébastien JEAN <contact@sebjean.com>
2022-01-22 14:04:17 +0100

    ✨ Add external link in game page

--- components/GameVersion.vue
+++ components/GameVersion.vue
@@ -6,6 +6,20 @@
             <span class="game-version-author">{{ $t('gameVersionBy', { author }) }}</span>
         </h1>
 
+        <a
+            :href="link"
+            class="game-version-game-link"
+            target="_blank"
+            rel="noopener"
+        >
+            <span class="sr-only">{{ $t('goToGame') }}</span>
+
+            <external-link-svg
+                class="game-version-link-icon"
+                aria-hidden
+            />
+        </a>
+
         <nuxt-link
             :to="localePath('/')"
             class="game-version-back-link"
@@ -16,9 +30,18 @@
 </template>
 
 <script>
+import ExternalLinkSvg from '~/assets/svg/external-link.svg?inline'
+
 export default {
+    components: {
+        ExternalLinkSvg,
+    },
     props: {
         author: {
+            type: String,
+            required: true,
+        },
+        link: {
             type: String,
             required: true,
         },
@@ -50,7 +73,16 @@ export default {
     &-title {
         margin-top: 0;
         margin-bottom: 0;
+        margin-right: .25em;
+    }
+
+    &-game-link {
+        display: inline-block;
+        vertical-align: middle;
+        width: 1.2em;
+        height: 1.2em;
         margin-right: .5em;
+        color: var(--color-headline);
     }
 }
 </style>
