1f5822141d9ce8295cfd244fa73069246a04e37d
Sébastien JEAN <contact@sebjean.com>
2022-01-22 18:55:42 +0100

    ✨ Add Sutom page

---
+++ pages/sutom.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <game-version v-bind="gameVersion" />
+
+        <wordle-grid
+            :min-count="6"
+            :max-count="9"
+            :fetch-dictionary="fetchDictionary"
+        />
+    </div>
+</template>
+
+<script>
+import gameVersions from '@/data/games.json'
+
+export default {
+    data() {
+        return {
+            gameVersion: gameVersions.sutom,
+        }
+    },
+    methods: {
+        async fetchDictionary(count) {
+            const { words } = await import(`@/data/dictionaries/fr-sutom/${count}.json`)
+            return words
+        },
+    },
+}
+</script>
